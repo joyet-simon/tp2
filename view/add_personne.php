1398f1c303c90968ad6768cfdae89bec32b6c7bf
joyet-simon <joyet.simon@hotmail.fr>
2019-01-18 12:20:06 +0100

    composer

--- view/add_personne.php
+++ view/add_personne.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Formulaire</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
         crossorigin="anonymous">
@@ -13,9 +13,9 @@
 </head>
 
 <body>
-    <form action="/add_personne_controller.php" onsubmit="return validateForm();"  method="post" id="form">
-        Prénom : <br><input type="text" name="prenom" id="prenom"><span id="prenomError"></span><br>
-        Nom : <br><input type="text" name="nom" id="nom"><span id="nomError"></span><br>
+    <form action="/add_personne_controller.php" onsubmit="return validateForm();"  method="post" id="form" class="form">
+        Prénom : <br><input type="text" name="prenom" id="prenom" class="form-control"><span id="prenomError"></span><br>
+        Nom : <br><input type="text" name="nom" id="nom" class="form-control"><span id="nomError"></span><br>
         <button class="btn btn-outline-info" >Valider</button>
     </form>
 </body>
